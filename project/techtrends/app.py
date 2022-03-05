7f6ae10a640577ac06c36c5397f822ef1fe4220f
Stephan Kluwe <stephan@creanetivity.com>
2022-03-05 11:21:00 +0100

    initial checkin before github-action setup

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,18 +1,30 @@
+import logging
 import sqlite3
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+# db connection count since start
+db_connection_count = 0
+# current number of posts (will be update on every index retrieval)
+number_of_posts = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+
+    global db_connection_count
+    db_connection_count += 1
+
     return connection
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
+
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
@@ -25,8 +37,13 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application 
 @app.route('/')
 def index():
+    global number_of_posts
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
+    number_of_posts = len(posts)
+
+    app.logger.debug('DEBUG: retrieved {} posts'.format(number_of_posts))
+
     connection.close()
     return render_template('index.html', posts=posts)
 
@@ -36,13 +53,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.warn('Non-existing page with id {} requested :('.format(post_id))
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article {} retrieved successfully!'.format(post['title']))
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('\"About us\" page retrieved !')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,10 +81,39 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.info('New article \"{}|" created!'.format(title))
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+@app.route('/healthz')
+def checkhealth():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Health request successful')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": number_of_posts}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.debug('Metrics request successfull')
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+# we don't want to use print for stdout
+   stdout_handler  = logging.StreamHandler(sys.stdout)
+   stdout_handler.setLevel(logging.INFO)
+
+   app.logger.addHandler(stdout_handler)
+
    app.run(host='0.0.0.0', port='3111')
